98b33b1626cac6ec72d84693aa2d0720c045c570
Felix Sosa <felixanthonysosa@gmail.com>
2017-03-12 13:50:56 -0400

    Completed Sims 1-3, added 4

--- main.py
+++ main.py
@@ -10,9 +10,9 @@ Simulations to implement:
 
 <Number>. <Factor> -- <Name> -- <Progress>
 
-1. Distance -- Long Distance -- In Progress
-2. Distance -- Short Distance -- In Progress
-3. Distance -- Medium Distance
+1. Distance -- Long Distance -- Complete/Edit
+2. Distance -- Short Distance -- Complete/Edit
+3. Distance -- Medium Distance -- Complete/Edit
 4. Force/Distance -- Static Cylinder
 5. Force -- Uphill Cylinder
 6. Force -- Downhill Cylinder
@@ -24,9 +24,10 @@ Simulations to implement:
 12. Duration -- Long Push Cylinder
 13. Duration -- Touch Cylinder
 
-Experiments to implement:
-
-...
+Progress Key:
+In Progress: May not be functional but working on it
+Complete/Edit: Functions as needed but may need edits or optimizations
+Complete: Functions and is optimal
 
 '''
 
@@ -36,21 +37,38 @@ import pymunk.pygame_util
 from pygame.locals import *
 import sys
 
-def addCone(space,x,y):
+def rem0(arbiter, space, data):
 	'''
-	Addsa a cone to the simulation at a given position.
+	Removes the Cone after colliding with the Fireball.
+	Expected that Cone is in space.shapes[1]
+	'''
+	space.remove(space.shapes[1])
+
+def rem1(arbiter, space, data):
+	'''
+	Stops Cylinder after colliding with Cone and applies (guestimated)
+	impulse to Cone. Expected that Cone is in shapes[1] and Cylinder 
+	is in shapes[2]
+	'''
+	space.shapes[2].body.velocity = (0,0)
+	space.shapes[1].body.apply_impulse_at_local_point((100,0))
+
+def addCone(space,x,y, imp=(0,0)):
+	'''
+	Adds a a [green cylinder] to the simulation at a given position.
 	space -- pymunk simulation space
 	x -- x coordinate
 	y -- y coordinate
 	'''
-	body = pymunk.Body(1,1)
+	body = pymunk.Body(100,1)
 	body.position = x,y
+	body.apply_impulse_at_local_point(imp)
 	shape = pymunk.Poly(body, [(0, 20),(20, -20),(-20, -20)])
 	shape.collision_type = 0
-	space.add(shape)
+	space.add(body, shape)
 	return shape
 
-def addCylinder(space, x, y, imp=(0,0)):
+def addCylinder(space, x, y, imp=(0,0), flag=True):
 	'''
 	Adds a cylinder [square] to teh simulation at a given position.
 	space -- pymunk simulation space
@@ -58,10 +76,14 @@ def addCylinder(space, x, y, imp=(0,0)):
 	y -- y coordinate
 	imp -- impulse for the cylinder
 	'''
-	body = pymunk.Body(10,1)
+	if flag:
+		body = pymunk.Body(10,1, body_type=pymunk.Body.DYNAMIC)
+	else:
+		body = pymunk.Body(body_type=pymunk.Body.STATIC)
 	body.position = x,y
 	body.apply_impulse_at_local_point(imp)
 	shape = pymunk.Poly.create_box(body, (30,30))
+	shape.collision_type = 1
 	space.add(body,shape)
 	return shape
 
@@ -74,11 +96,11 @@ def addFireball(space, x, y, rad=15):
 	rad -- circle radius
 	'''
 	body = pymunk.Body(body_type=pymunk.Body.STATIC)
-	body.position = x,y # 3
+	body.position = x,y 
 	shape = pymunk.Circle(body, rad)
 	shape.color = pygame.color.THECOLORS["red"]
-	shape.collision_type = 1
-	space.add(body,shape) # 5
+	shape.collision_type = 2
+	space.add(body,shape) 
 	return shape
 
 def longDistanceSim(space, screen, options):
@@ -89,12 +111,20 @@ def longDistanceSim(space, screen, options):
 	screen -- pygame display Surface
 	options -- draw options for pymunk space
 	'''
+	pygame.display.set_caption("Simulation 1: Long Distance")
+	# set up collision handlers
+	ch0 = space.add_collision_handler(0, 2)
+	ch0.data["surface"]=screen
+	ch0.post_solve=rem0
+	ch1 = space.add_collision_handler(0, 1)
+	ch1.data["surface"]=screen
+	ch1.post_solve=rem1
 
 	# add shapes
 	ball = addFireball(space, 500, 300)
 	cone = addCone(space, 400, 300)
 	cylinder = addCylinder(space, 100, 300, (1000,0))
-	
+
 	while True:
 		#allow user to exit
 		for event in pygame.event.get():
@@ -105,13 +135,11 @@ def longDistanceSim(space, screen, options):
 
 		# set clock
 		clock = pygame.time.Clock()
-		
 		# setup display and run sim
 		screen.fill((255,255,255))
 		space.step(1/50.0)
 		space.debug_draw(options)
 		pygame.display.flip()
-		pygame.display.set_caption("Simulation 1: Long Distance")
 		clock.tick(50)
 
 def shortDistanceSim(space, screen, options):
@@ -122,10 +150,19 @@ def shortDistanceSim(space, screen, options):
 	screen -- pygame display Surface
 	options -- draw options for pymunk space
 	'''
+	pygame.display.set_caption("Simulation 2: Short Distance")
+	# set up collision handlers
+	ch0 = space.add_collision_handler(0, 2)
+	ch0.data["surface"]=screen
+	ch0.post_solve=rem0
+	ch1 = space.add_collision_handler(0, 1)
+	ch1.data["surface"]=screen
+	ch1.post_solve=rem1
+
 	# add shapes
-	ball = addFireball(space, 400, 300)
-	cone = addCone(space, 300, 300)
-	cylinder = addCylinder(space, 150, 300, (1000,0))
+	ball = addFireball(space, 500, 300)
+	cone = addCone(space, 400, 300)
+	cylinder = addCylinder(space, 250, 300, (1000,0))
 
 	while True:
 		#allow user to exit
@@ -143,7 +180,73 @@ def shortDistanceSim(space, screen, options):
 		space.step(1/50.0)
 		space.debug_draw(options)
 		pygame.display.flip()
-		pygame.display.set_caption("Simulation 2: Short Distance")
+		clock.tick(50)
+
+def mediumDistanceSim(space, screen, options):
+	'''
+	Simulation of Cylinder pushing Cone into Fireball from a long distance
+	away. Originally to be compared with shortDistanceSim in Moral Kinematics.
+	space -- pymunk simulation space
+	screen -- pygame display Surface
+	options -- draw options for pymunk space
+	'''
+	pygame.display.set_caption("Simulation 3: Medium Distance")
+	# set up collision handlers
+	ch0 = space.add_collision_handler(0, 2)
+	ch0.data["surface"]=screen
+	ch0.post_solve=rem0
+	ch1 = space.add_collision_handler(0, 1)
+	ch1.data["surface"]=screen
+	ch1.post_solve=rem1
+
+	# add shapes
+	ball = addFireball(space, 500, 300)
+	cone = addCone(space, 400, 300)
+	cylinder = addCylinder(space, 175, 300, (1000,0))
+
+	while True:
+		#allow user to exit
+		for event in pygame.event.get():
+			if event.type == QUIT:
+				sys.exit(0)
+			elif event.type == KEYDOWN and event.key == K_ESCAPE:
+				sys.exit(0)
+
+		# set clock
+		clock = pygame.time.Clock()
+		# setup display and run sim
+		screen.fill((255,255,255))
+		space.step(1/50.0)
+		space.debug_draw(options)
+		pygame.display.flip()
+		clock.tick(50)
+
+def staticCylinder(space, screen, options):
+	pygame.display.set_caption("Simulation 4: Static Cylinder")
+	# set up collision handlers
+	ch0=space.add_collision_handler(0,2)
+	ch0.data["surface"]=screen
+	ch0.post_solve=rem0
+
+	# add shapes
+	ball = addFireball(space, 500, 300)
+	cone = addCone(space, 200, 300, (10000,5500))
+	cylinder = addCylinder(space, 350, 400, (0,0), False)
+
+	while True:
+		# allow user to exit
+		for event in pygame.event.get():
+			if event.type == QUIT:
+				sys.exit(0)
+			elif event.type == KEYDOWN and event.key == K_ESCAPE:
+				sys.exit(0)
+
+		# setup display and run sim
+		clock = pygame.time.Clock()
+		screen.fill((255,255,255))
+		space.step(1/50.0)
+		space.debug_draw(options)
+		pygame.display.flip()
 		clock.tick(50)
 
 def main():
@@ -152,7 +255,7 @@ def main():
 	'''
 	
 	# list experiment options to user
-	print("Please choose a Simulation [1-13]:")
+	print("Please choose a Simulation [1-13] or [0] to exit:")
 	choice = raw_input()
 
 	# initialize pygame and create a space to contain the simulation
@@ -162,9 +265,8 @@ def main():
 	# create a screen of 600x600 pixels
 	screen = pygame.display.set_mode((600,600))	
 	drawOptions = pymunk.pygame_util.DrawOptions(screen)
-	
-	#longDistanceSim(space, screen, drawOptions)
-	shortDistanceSim(space, screen, drawOptions)
+
+	staticCylinder(space, screen, drawOptions)
 
 if __name__ == '__main__':
 	sys.exit(main())
