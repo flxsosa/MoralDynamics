85270554da2b6e1c6ef9dc7196cf6724ba43fa7b
Felix Sosa <felixanthonysosa@gmail.com>
2017-04-02 10:32:10 -0400

    completed basic terminal menu for sims

--- main.py
+++ main.py
@@ -28,15 +28,50 @@ def main():
 	print("Please choose a Simulation [1-13] or [0] to exit:")
 	choice = raw_input()
 	
-	# initialize pygame and create a space to contain the simulation
-	pygame.init()
-	space = pymunk.Space()
+	while (choice != '0'):
+		# initialize pygame and create a space to contain the simulation
+		pygame.init()
+		space = pymunk.Space()
 
-	# create a screen of 600x600 pixels
-	screen = pygame.display.set_mode((600,600))	
-	drawOptions = pymunk.pygame_util.DrawOptions(screen)
+		# create a screen of 600x600 pixels
+		screen = pygame.display.set_mode((600,600))	
+		drawOptions = pymunk.pygame_util.DrawOptions(screen)
 
-	simulations.test(space, screen, drawOptions)
+		if (choice == '1'):
+			simulations.shortDistance(space, screen, drawOptions)
+		elif (choice == '2'):
+			simulations.mediumDistance(space, screen, drawOptions)
+		elif (choice == '3'):
+			simulations.longDistance(space, screen, drawOptions)
+		elif (choice == '4'):
+			simulations.static(space, screen, drawOptions)
+		elif (choice == '5'):
+			simulations.uphill(space, screen, drawOptions)
+		elif (choice == '6'):
+			simulations.downhill(space, screen, drawOptions)
+		elif (choice == '7'):
+			simulations.slowCollision(space, screen, drawOptions)
+		elif (choice == '8'):
+			simulations.fastCollision(space, screen, drawOptions)
+		elif (choice == '9'):
+			simulations.dodge(space, screen, drawOptions)
+		elif (choice == '10'):
+			simulations.doubleTouch(space, screen, drawOptions)
+		elif (choice == '11'):
+			simulations.mediumPush(space, screen, drawOptions)
+		elif (choice == '12'):
+			simulations.longPush(space, screen, drawOptions)
+		elif (choice == '13'):
+			simulations.touch(space, screen, drawOptions)
+		elif (choice == '14'):
+			simulations.test(space, screen, drawOptions)
 
+		pygame.quit()
+		print("Please choose a Simulation [1-13] or [0] to exit:")
+		choice = raw_input()
+		
+	print("Goodbye")
+	sys.exit()
+	
 if __name__ == '__main__':
 	sys.exit(main())
