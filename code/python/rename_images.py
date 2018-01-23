9a70646a22bf1999917f5072a02dd2dfbb85ba99
Felix Sosa <felixanthonysosa@gmail.com>
2018-01-23 11:18:41 -0500

    init of script that renames images

---
+++ code/python/rename_images.py
@@ -0,0 +1,32 @@
+'''
+Renames images output from rendering on om in Moral Dynamics.
+
+January 21, 2018
+author: Felix Sosa
+'''
+import os
+import re
+
+for dirr, subdirs, files in os.walk("../../../../Desktop/images"):
+	# Traverse the files
+	for file in files:
+		print(dirr,file)
+		# Make sure file is an image.png
+		match = re.match(r"([a-z]+)([0-9]+).([a-z]+)", file, re.I)
+		if match:
+			# Parse the filename into types
+			items = match.groups()
+			# If the image is of an animation
+			if len(str(items[1])) > 3:
+				if (len(str(items[1])) == 7):
+					idx = int(str(items[1])[:3])
+					inc = int(str(items[1])[3:])
+					new_idx = idx+inc
+					filename = "image"+str(new_idx)+".png"
+					os.rename(dirr+"/"+file,dirr+"/"+filename)
+				elif (len(str(items[1])) == 6):
+					idx = int(str(items[1])[:2])
+					inc = int(str(items[1])[2:])
+					new_idx = idx+inc
+					filename = "image"+str(new_idx)+".png"
+					os.rename(dirr+"/"+file,dirr+"/"+filename)
