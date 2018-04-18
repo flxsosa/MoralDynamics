b0a5ed130c835bdec90258ded32187cfa5914c3f
twizzler <twizzler@dhcp-18-111-16-230.dyn.MIT.EDU>
2018-04-18 15:32:45 -0400

    removed comments

--- code/om/animation.py
+++ code/om/animation.py
@@ -144,10 +144,7 @@ def init():
     fireball = bpy.data.objects.get('fireball')
     plane = bpy.data.objects.get('plane')
 
-<<<<<<< HEAD
-=======
     # Move agents around scene and render each frame and save it as an image
->>>>>>> a980930a3ca1ec7c463b86f40a2d4d0df38ef277
     for idx in range(simulation_data['config']['ticks']):
         # Move agent
         agent.location.x = (simulation_data['objects']['agent'][idx]['x']/100.0)
